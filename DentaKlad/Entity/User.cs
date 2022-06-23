a6d19bf5e26319c27df9b2178dfc2559c50047aa
Arina Shvydkaya <fhbyf4@mail.ru>
2022-06-23 16:17:14 +0300

    Изменено название проекта и установлен EntityFramework

---
+++ DentaKlad/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentaKlad.Entity
+{
+    //Класс пользователей для работы в приложении
+    internal class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
