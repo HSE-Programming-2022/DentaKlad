b005fd47c9b21b6caa828158556c90fc658db774
nebel0ng <heudyxg@gmail.com>
2022-06-25 15:54:39 +0300

    Create для Appointment

--- DentaKlad.Design/Appointments.xaml.cs
+++ DentaKlad.Design/Appointments.xaml.cs
@@ -18,9 +18,17 @@ namespace DentaKlad.Design
     public partial class Appointments : Window
     {
         Context context;
+        List<ListBox> listBoxes;
         public Appointments()
         {
             InitializeComponent();
+            listBoxes = new List<ListBox>
+            {
+                Clients,
+                Doctors,
+                Services
+            };
+            DateTimeBox.Clear();
             LoadData();
         }
 
@@ -35,6 +43,10 @@ namespace DentaKlad.Design
 
             AptTable.ItemsSource = linqQuery.ToList();
 
+            Clients.ItemsSource = context.Clients.Select(c => c.Name).ToList();
+            Doctors.ItemsSource = context.Doctors.Select(d => d.Name).ToList();
+            Services.ItemsSource = context.Services.Select(s => s.Name).ToList();
+
         }
 
         private void Button_Click_Delete(object sender, RoutedEventArgs e)
@@ -46,8 +58,32 @@ namespace DentaKlad.Design
                 context.SaveChanges();
                 LoadData();
                 //MessageBox.Show(AptTable.SelectedItem.ToString().Split()[3].TrimEnd(','));
-                
 
+
+            }
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            if (!DateTime.TryParse(DateTimeBox.Text, out DateTime dt) | dt <= DateTime.Now)
+            {
+                DateTimeBox.Clear();
+            }
+            if (!listBoxes.Where(b => b.SelectedItem == null).Any() & !String.IsNullOrEmpty(DateTimeBox.Text))
+            {
+                var newApt = new Appointment(context.Clients.Where(c => c.Name == Clients.SelectedItem).FirstOrDefault(), context.Services.Where(s => s.Name == Services.SelectedItem).FirstOrDefault(), context.Doctors.Where(d => d.Name == Doctors.SelectedItem).FirstOrDefault(), DateTimeBox.Text);
+
+                if (context.Appointments.Where(a => a.DateAndTime == newApt.DateAndTime & a.Client.Name == newApt.Client.Name & a.Doctor.Name == newApt.Doctor.Name & a.Service.Name == newApt.Service.Name).Any())
+                {
+                    MessageBox.Show("Информация об этой записи уже есть.");
+                }
+                else
+                {
+                    DateTimeBox.Clear();
+                    context.Appointments.Add(newApt);
+                    context.SaveChanges();
+                    LoadData();
+                }
             }
         }
     }
