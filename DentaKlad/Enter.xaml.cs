1786f125e0fae8fb92e69fb4f02a06d54921803c
IlyaNazarev <nazarev.ilya2002@gmail.com>
2022-06-26 18:49:56 +0300

    Полностью доработал регистрацию, без подсоединения бд

--- DentaKlad/Enter.xaml.cs
+++ DentaKlad/Enter.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DentaKlad.Entity;
+using System.Windows.Navigation;
 
 namespace DentaKlad
 {
@@ -19,8 +21,52 @@ namespace DentaKlad
     /// </summary>
     public partial class Enter : Window
     {
+        User user = new User("Yura Sanochkin", "thebest");
         public Enter()
         {
+            InitializeComponent();
+        }
+
+        private void Button_enter(object sender, RoutedEventArgs e)
+        {
+            string username = login.Text;
+            string parol = password.Password;
+            if (username.Length == 0 || parol.Length == 0)
+            {
+                login.Foreground = Brushes.Red;
+                password.Foreground = Brushes.Red;
+                hint.Foreground = Brushes.Red;
+                hint.Text = "Данные заполнены не полностью";
+            }
+            else if (login.Text != user.username || password.Password != user.password)
+            {
+                login.Text = "";
+                password.Password = "";
+                login.Foreground = Brushes.Red;
+                password.Foreground = Brushes.Red;
+                hint.Foreground = Brushes.Red;
+                hint.Text = "Данные заполнены некорректно";
+            }
+            else if (login.Text == user.username || password.Password == user.password)
+            {
+                login.Foreground = Brushes.Green;
+                password.Foreground = Brushes.Green;
+                hint.Foreground = Brushes.Green;
+                hint.Text = "Данные заполнены корректно!";
+            }
+        }
+
+        private void Button_help(object sender, RoutedEventArgs e) 
+        {
+            MessageBox.Show($"Login: {user.username}{Environment.NewLine}Password: {user.password}", "Помощь", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MouseDownstackpanel(object sender, MouseButtonEventArgs e)
+        {
+            login.Foreground = Brushes.Black;
+            password.Foreground = Brushes.Black;
+            hint.Foreground = Brushes.Gray;
+            hint.Text = "Забыли логин и пароль? Нажмите кнопку Помощь";
         }
     }
 }
