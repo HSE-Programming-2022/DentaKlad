28f2d7af7f2b01f291dfa3125a77cc5e4a1e0e1b
IlyaNazarev <nazarev.ilya2002@gmail.com>
2022-06-28 18:56:14 +0300

    dk-19 Регистрация полностью доделана

--- DentaKlad/Enter.xaml.cs
+++ DentaKlad/Enter.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Media;
 using DentaKlad.Core;
 using DentaKlad.Core.Entity;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace DentaKlad.Design
 {
@@ -13,33 +14,30 @@ namespace DentaKlad.Design
     /// </summary>
     public partial class Enter : Window
     {
-        ApplicationContext db;
+        ApplicationContext db = new ApplicationContext();
         public Enter()
         {
             InitializeComponent();
-            using (db = new ApplicationContext())
+            /*using (db = new ApplicationContext())
             {
                 var user1 = new User();
                 user1.Username = "test";
-                user1.Password = "test";
+                user1.Password = md5.hashPassword("test");
 
-                /*var user2 = new User()
-                {
-                    Username = "1234",
-                    Password = "1234"
-                };*/
+                var user2 = new User();
+                user2.Username = "1234";
+                user2.Password = md5.hashPassword("1234");
                 db.Users.Add(user1);
-                /*db.Users.Add(user2);*/
+                db.Users.Add(user2);
                 db.SaveChanges();
-            }
+            }*/
         }
 
         private void Button_enter(object sender, RoutedEventArgs e)
         {
             string username = login.Text;
-            string parol = password.Password;
-
-            if (username.Length == 0 || parol.Length == 0)
+            string parol = md5.hashPassword(password.Password);
+            if (username.Length == 0 || password.Password.Length == 0)  
             {
                 login.Foreground = Brushes.Red;
                 password.Foreground = Brushes.Red;
@@ -70,8 +68,8 @@ namespace DentaKlad.Design
         
         private void Button_help(object sender, RoutedEventArgs e) 
         {
-            var usname = db.Users.First();
-            var paswrd = db.Users.First();
+            var usname = "1234";
+            var paswrd = "1234";
             MessageBox.Show($"Login: {usname}{Environment.NewLine}Password: {paswrd}", "Помощь", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
